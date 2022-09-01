73674e57f6670031230c7e7d1ff699b9e252cbc7
Gary Villar <Villar.Gary.22@gmail.com>
2022-08-31 18:49:32 -0700

    first commit

---
+++ ChessPiece.hpp
@@ -0,0 +1,39 @@
+#ifndef _CHESSPIECE_HPP_
+#define _CHESSPIECE_HPP_
+
+#include <string>
+#include <utility>
+#include <vector>
+
+// Abstract base class that every chess piece will inherit from this parent class
+// is meant purely to be inherited from and to leverager polymorphism
+// so that we can treat any more specific chass piece the same
+
+
+class ChessPiece{
+    private:
+        std::string icon;
+        std::string color;
+        std::pair<int,int> position;
+
+    
+    public:
+        // So far it seems the only thing that is different between the different
+        // classes of chessPieces is that they generate different moves.
+        ChessPiece(std::string i, std::string c, std::pair<int,int> p) {icon = i; color = c; position = p; };
+        
+        void setName( std::string i) { icon = i; } ;
+        std::string getName() const { return icon; };
+
+        void setColor( std::string c) { color = c; } ;
+        std::string getColor() const { return color; };
+
+        void setPosition( std::pair<int,int> p ) { position = p; };
+        std::pair<int,int> getPosition() { return position; };
+
+        virtual std::vector<std::pair<int,int>> generateMoves() = 0;
+};
+
+
+
+#endif
